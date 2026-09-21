c8211cf492326d2341c530e09321d11f85fcb522
konzo <bogrek2011@gmail.com>
2026-09-21 18:46:25 +0700

    feat: add WorldServer skeleton

---
+++ server/WorldServer/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/health", () =>
+    Results.Ok(new
+    {
+        status = "ok",
+        service = "WorldServer"
+    }));
+
+app.Run();
